b71b3a299e95b1d15e222c68f269750623fed359
maker2413 <maker2413@gmail.com>
2024-07-09 12:18:21 -0500

    began building out python file with org-mode

---
+++ PackagingMLModels/loan.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder,MinMaxScaler
+
+raw_train = pd.read_csv("train.csv")
+raw_test = pd.read_csv("test.csv")
+
+train_df = raw_train.copy()
+test_df = raw_test.copy()
+
+train_df.info()
